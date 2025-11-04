8d79e604e5bc1c9ffb1bded2793f81bc22f72b0e
ImDongHan <dim95985@gmail.com>
2025-11-04 12:08:56 +0900

    first commit
    first commit

---
+++ src/main/java/com/example/BoardProject_back/BoardProjectBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.BoardProject_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardProjectBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardProjectBackApplication.class, args);
+	}
+
+}
