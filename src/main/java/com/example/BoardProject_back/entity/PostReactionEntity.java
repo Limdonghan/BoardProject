eb3eec89a1e3b64317df510144c31dd227e1aeec
ImDongHan <dim95985@gmail.com>
2025-11-25 09:58:16 +0900

    주석 정리
    주석 정리

--- src/main/java/com/example/BoardProject_back/entity/PostReactionEntity.java
+++ src/main/java/com/example/BoardProject_back/entity/PostReactionEntity.java
@@ -10,18 +10,19 @@ import lombok.*;
 @AllArgsConstructor
 public class PostReactionEntity {
 
+    /// 한 사람이 한 게시글에 중복으로 좋아요 누르는 것을 DB 차원에서도 막기 위해 설정
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id")
-    private PostEntity post; // 어떤 글인지
+    private PostEntity post; /// 어떤 글인지
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
-    private UserEntity user; // 누가 눌렀는지
+    private UserEntity user; /// 누가 눌렀는지
 
     @Column(nullable = false)
-    private String reactionType; // "LIKE" 또는 "DISLIKE"
+    private String reactionType; /// "LIKE" 또는 "DISLIKE"
 }
