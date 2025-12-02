f7d7b4a075b7dd9d6a81a1f2799331cb33ab7eb7
ImDongHan <dim95985@gmail.com>
2025-12-02 16:48:01 +0900

    feat(report): 신고 기능에 중복 및 본인 신고 방지 로직 추가
    - 동일한 게시글/댓글에 대한 중복 신고를 방지합니다.
    - 사용자가 자신의 게시글/댓글을 신고할 수 없도록 제한합니다.

--- src/main/java/com/example/BoardProject_back/repository/ReportRepository.java
+++ src/main/java/com/example/BoardProject_back/repository/ReportRepository.java
@@ -19,9 +19,11 @@ public interface ReportRepository extends JpaRepository<ReportEntity,Integer> {
 
     /// 해당 게시글 신고자 리스트
     List<ReportEntity> findAllByPostId(int postId);
-    
+
     /// 해당 댓글 신고자 리스트
     List<ReportEntity> findAllByCommentId(int commentId);
 
-
+    /// 유저가 게시글 or 댓글에 신고를 한 적 있는지 체크(한번만 가능하도록)
+    boolean existsByReporterIdAndPostId(int reporterId, int postId);
+    boolean existsByReporterIdAndCommentId(int reporterId, int commentId);
 }
